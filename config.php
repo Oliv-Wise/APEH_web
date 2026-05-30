9d8b5dff5774205a03a5e87a51b28ce89549c7b4
Oliver Joseph <marvensoliver.joseph.etu@univ-lille.fr>
2026-05-30 04:39:42 +0200

    Prepare Laravel Cloud deployment

--- config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;dbname=apeh_france;charset=utf8mb4', 'root', '');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
--- db/includes/env.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = 'localhost';
-$db   = 'apeh';
-$user = 'postgres'; 
-$pass = '';        
-$dsn = "pgsql:host=$host;dbname=$db";
