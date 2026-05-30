2dc856f66fb100aec2216919e9d79be80c004e02
Oliver Joseph <marvensoliver.joseph.etu@univ-lille.fr>
2026-05-30 21:19:22 +0200

    Remove automatic migration from app boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,10 +15,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useTailwind();
-
-        // Force la création des tables si la base PostgreSQL du Cloud est vide
-        if (config('app.env') === 'production' && !Schema::hasTable('articles')) {
-            Artisan::call('migrate', ['--force' => true]);
-        }
     }
 }
